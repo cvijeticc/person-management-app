fa75ac27057e419aac6f53c281b3fb6a7a23c7e6
andri <marko.andrija@yahoo.com>
2026-08-10 09:52:00 +0200

    Ucitavanje osoba sa API-ja preko axios-a i useEffect-a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 
+const API_URL = 'http://localhost:3001/persons';
+
 function App() {
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    loadPersons();
+  }, []);
+
+  function loadPersons() {
+    axios.get(API_URL).then((response) => {
+      setPersons(response.data);
+    });
+  }
+
   return (
     <div className="app">
       <Header />
@@ -10,6 +26,7 @@ function App() {
         <Navigation />
         <div className="content">
           <h2>Osobe</h2>
+          <p>Ucitano osoba: {persons.length}</p>
         </div>
       </div>
     </div>
