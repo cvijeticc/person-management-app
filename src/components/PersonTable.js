728cef6872708b3e6e160c43adea3ad5fc6d759e
andri <marko.andrija@yahoo.com>
2026-08-14 10:48:00 +0200

    Tabela prebacena na Fluent UI DetailsList

--- src/components/PersonTable.js
+++ src/components/PersonTable.js
@@ -1,43 +1,69 @@
+import {
+  DetailsList,
+  SelectionMode,
+  DefaultButton,
+  Stack,
+} from '@fluentui/react';
+
+// DetailsList ne cita polja sam - mora mu se opisati svaka kolona
+const columns = [
+  { key: 'id', name: 'Id', fieldName: 'id', minWidth: 25, maxWidth: 35 },
+  { key: 'name', name: 'Ime', fieldName: 'name', minWidth: 70, maxWidth: 100 },
+  {
+    key: 'surname',
+    name: 'Prezime',
+    fieldName: 'surname',
+    minWidth: 80,
+    maxWidth: 110,
+  },
+  {
+    key: 'userType',
+    name: 'Tip korisnika',
+    fieldName: 'userType',
+    minWidth: 90,
+    maxWidth: 110,
+  },
+  {
+    key: 'createdDate',
+    name: 'Datum kreiranja',
+    fieldName: 'createdDate',
+    minWidth: 100,
+    maxWidth: 110,
+  },
+  { key: 'city', name: 'Grad', fieldName: 'city', minWidth: 70, maxWidth: 100 },
+  {
+    key: 'address',
+    name: 'Adresa',
+    fieldName: 'address',
+    minWidth: 130,
+    maxWidth: 180,
+  },
+  { key: 'actions', name: 'Akcije', minWidth: 150 },
+];
+
 function PersonTable({ persons, onEdit, onDelete }) {
+  // poziva se za svaku celiju - za kolonu "actions" vracamo dugmad,
+  // a za sve ostale obicnu vrednost iz objekta
+  function renderItemColumn(person, index, column) {
+    if (column.key === 'actions') {
+      return (
+        <Stack horizontal tokens={{ childrenGap: 8 }}>
+          <DefaultButton text="Izmeni" onClick={() => onEdit(person)} />
+          <DefaultButton text="Obrisi" onClick={() => onDelete(person.id)} />
+        </Stack>
+      );
+    }
+    return person[column.fieldName];
+  }
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Ime</th>
-          <th>Prezime</th>
-          <th>Tip korisnika</th>
-          <th>Datum kreiranja</th>
-          <th>Grad</th>
-          <th>Adresa</th>
-          <th>Akcije</th>
-        </tr>
-      </thead>
-      <tbody>
-        {persons.map((person) => (
-          <tr key={person.id}>
-            <td>{person.id}</td>
-            <td>{person.name}</td>
-            <td>{person.surname}</td>
-            <td>{person.userType}</td>
-            <td>{person.createdDate}</td>
-            <td>{person.city}</td>
-            <td>{person.address}</td>
-            <td>
-              <button className="edit-button" onClick={() => onEdit(person)}>
-                Izmeni
-              </button>
-              <button
-                className="delete-button"
-                onClick={() => onDelete(person.id)}
-              >
-                Obrisi
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <DetailsList
+      items={persons}
+      columns={columns}
+      getKey={(person) => person.id}
+      selectionMode={SelectionMode.none}
+      onRenderItemColumn={renderItemColumn}
+    />
   );
 }
 
